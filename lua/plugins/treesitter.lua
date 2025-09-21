5b079f2320e585f4ae3edccf4d1c9fba863a580a
ezra <ezralawrence02@gmail.com>
2025-09-21 00:48:49 -0500

    chore(nvim): enhance treesitter setup - Added parsers for json, yaml, html, css, javascript, typescript - Enabled auto_install for missing parsers - Configured incremental selection keymaps - Set Treesitter as default foldexpr

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-local M = {
+llocal M = {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
@@ -6,10 +6,49 @@ local M = {
 
 function M.config()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" },
-    highlight = { enable = true },
+    -- Parsers to install
+    ensure_installed = {
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "python",
+      "json",
+      "yaml",
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+    },
+
+    sync_install = false,   -- install parsers asynchronously
+    auto_install = true,    -- install missing parsers automatically
+
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+
     indent = { enable = true },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        scope_incremental = "<S-CR>",
+        node_decremental = "<BS>",
+      },
+    },
+
+    -- enable if you install nvim-ts-autotag
+    autotag = { enable = true },
   }
+
+  -- Treesitter-based folding
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 end
 
 return M
+
