53e03236860cf9898aca6e10675cc9bab5dd10b1
ezra <ezralawrence02@gmail.com>
2025-09-20 23:50:31 -0500

    first commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,59 @@
+-- Leader keys (must be set before mappings)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Disable <Space> default mapping
+keymap("", "<Space>", "<Nop>", opts)
+
+-- Better window navigation with Alt
+keymap("n", "<M-h>", "<C-w>h", opts)
+keymap("n", "<M-j>", "<C-w>j", opts)
+keymap("n", "<M-k>", "<C-w>k", opts)
+keymap("n", "<M-l>", "<C-w>l", opts)
+keymap("n", "<M-Tab>", "<C-6>", opts) -- switch buffers
+
+-- Center search results
+keymap("n", "n", "nzz", opts)
+keymap("n", "N", "Nzz", opts)
+keymap("n", "*", "*zz", opts)
+keymap("n", "#", "#zz", opts)
+keymap("n", "g*", "g*zz", opts)
+keymap("n", "g#", "g#zz", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Replace selection without yanking
+keymap("x", "p", [["_dP]], opts)
+
+-- Custom LSP popup menu
+vim.cmd [[amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definition()<CR>]]
+vim.cmd [[amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
+-- vim.cmd [[amenu 10.120 mousemenu.-sep- *]]
+
+keymap("n", "<RightMouse>", "<cmd>popup mousemenu<CR>", opts)
+-- Optional: <Tab> to open popup menu
+-- keymap("n", "<Tab>", "<cmd>popup mousemenu<CR>", opts)
+
+-- Easier motions to line edges
+keymap({ "n", "o", "x" }, "<S-h>", "^", opts)
+keymap({ "n", "o", "x" }, "<S-l>", "g_", opts)
+
+-- Better j/k with wrapped lines, but preserve counts
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap("x", "j", "gj", opts)
+keymap("x", "k", "gk", opts)
+
+-- Toggle line wrapping
+keymap("n", "<leader>w", function()
+  vim.wo.wrap = not vim.wo.wrap
+end, opts)
+
+-- Escape terminal mode with Ctrl-;
+keymap("t", "<C-;>", [[<C-\><C-n>]], opts)
+
---
+++ lua/core/options.lua
@@ -0,0 +1,77 @@
+-- Neovim Options
+
+local opt = vim.opt
+local g = vim.g
+
+-- File handling
+opt.backup = false
+opt.swapfile = false
+opt.writebackup = false
+opt.undofile = true -- persistent undo
+
+-- UI
+opt.termguicolors = true
+opt.signcolumn = "yes:1" -- always show, but only 1 column
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 4
+opt.laststatus = 3 -- global statusline
+opt.showmode = false
+opt.showtabline = 1
+opt.title = false
+opt.wrap = false
+opt.colorcolumn = "" -- you can set "80" or "120" if you want
+opt.fillchars:append { eob = " ", stl = " " }
+
+-- Command line / messages
+opt.cmdheight = 0 -- auto-hide cmdline (needs Neovim 0.9+)
+opt.showcmd = false
+opt.ruler = false
+opt.shortmess:append "c"
+
+-- Search
+opt.hlsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Completion / menus
+opt.completeopt = { "menuone", "noselect" }
+opt.pumheight = 10
+opt.pumblend = 10
+
+-- Indentation
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+
+-- Splits
+opt.splitbelow = true
+opt.splitright = true
+
+-- Performance
+opt.timeoutlen = 1000
+opt.updatetime = 100
+
+-- Scrolling
+opt.scrolloff = 4 -- keep some context around cursor
+opt.sidescrolloff = 8
+
+-- Mouse / clipboard
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+
+-- Fonts (for GUI clients)
+opt.guifont = "monospace:h17"
+
+-- Keywords
+opt.iskeyword:append("-")
+
+-- Wrapping
+opt.whichwrap:append "<,>,[,],h,l"
+
+-- Netrw (if you use it)
+g.netrw_banner = 0
+g.netrw_mouse = 2
+
