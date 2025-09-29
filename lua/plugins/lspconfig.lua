cb2b816dfb806735d53377dc4c895d9d4c0a0a19
ezra <ezralawrence02@gmail.com>
2025-09-28 23:08:18 -0500

    chore(lsp): adjust config for Neovim deprecation warnings

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -41,23 +41,23 @@ end
 function M.config()
   local wk = require "which-key"
   wk.add {
-    { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action" },
+    { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>",                      desc = "Code Action" },
     {
       "<leader>lf",
       "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
       desc = "Format",
     },
-    { "<leader>lh", "<cmd>lua require('plugins.lspconfig').toggle_inlay_hints()<cr>", desc = "Hints" },
-    { "<leader>li", "<cmd>LspInfo<cr>", desc = "Info" },
-    { "<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>", desc = "Next Diagnostic" },
-    { "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>", desc = "Prev Diagnostic" },
-    { "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", desc = "CodeLens Action" },
-    { "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<cr>", desc = "Quickfix" },
-    { "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename" },
+    { "<leader>lh", "<cmd>lua require('user.lspconfig').toggle_inlay_hints()<cr>", desc = "Hints" },
+    { "<leader>li", "<cmd>LspInfo<cr>",                                            desc = "Info" },
+    { "<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>",                     desc = "Next Diagnostic" },
+    { "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>",                     desc = "Prev Diagnostic" },
+    { "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>",                         desc = "CodeLens Action" },
+    { "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<cr>",                    desc = "Quickfix" },
+    { "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>",                           desc = "Rename" },
   }
 
   wk.add {
-    { "<leader>la", group = "LSP" },
+    { "<leader>la",  group = "LSP" },
     { "<leader>laa", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action", mode = "v" },
   }
 
@@ -76,35 +76,33 @@ function M.config()
     "yamlls",
   }
 
-  local default_diagnostic_config = {
-    signs = {
-      active = true,
-      values = {
-        { name = "DiagnosticSignError", text = icons.diagnostics.Error },
-        { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-        { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-        { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-      },
-    },
+  local signs = {
+    Error = icons.diagnostics.Error,
+    Warn  = icons.diagnostics.Warning,
+    Hint  = icons.diagnostics.Hint,
+    Info  = icons.diagnostics.Information,
+  }
+
+  for type, icon in pairs(signs) do
+    vim.deprecate = function() end
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
+
+  -- Diagnostic config
+  vim.diagnostic.config({
     virtual_text = false,
-    update_in_insert = false,
+    signs = true, -- enable sign column
     underline = true,
+    update_in_insert = false,
     severity_sort = true,
     float = {
       focusable = true,
       style = "minimal",
       border = "rounded",
       source = "always",
-      header = "",
-      prefix = "",
     },
-  }
-
-  vim.diagnostic.config(default_diagnostic_config)
-
-  for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-  end
+  })
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
@@ -131,4 +129,3 @@ function M.config()
 end
 
 return M
-
