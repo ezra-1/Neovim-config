0db7dcdede4459f5a7016145c18b3767f9ba9f5d
ezra <ezralawrence02@gmail.com>
2025-10-04 08:29:58 -0500

    fix(lsp): correct or adjust inline hint display

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -47,7 +47,7 @@ function M.config()
       "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
       desc = "Format",
     },
-    { "<leader>lh", "<cmd>lua require('user.lspconfig').toggle_inlay_hints()<cr>", desc = "Hints" },
+    { "<leader>lh", "<cmd>lua require('plugins.lspconfig').toggle_inlay_hints()<cr>", desc = "Hints" },
     { "<leader>li", "<cmd>LspInfo<cr>",                                            desc = "Info" },
     { "<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>",                     desc = "Next Diagnostic" },
     { "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>",                     desc = "Prev Diagnostic" },
