b51d41e8ba2c1f83838be73b3a2b00fa78b52529
ezra <ezralawrence04@gmail.com>
2025-10-09 02:41:58 -0400

    feat(lsp): add keybinding to show hint diagnostics in floating window

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -41,19 +41,19 @@ end
 function M.config()
   local wk = require "which-key"
   wk.add {
-    { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>",                      desc = "Code Action" },
+    { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>",                                                 desc = "Code Action" },
     {
       "<leader>lf",
       "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
       desc = "Format",
     },
-    { "<leader>lh", "<cmd>lua require('plugins.lspconfig').toggle_inlay_hints()<cr>", desc = "Hints" },
-    { "<leader>li", "<cmd>LspInfo<cr>",                                            desc = "Info" },
-    { "<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>",                     desc = "Next Diagnostic" },
-    { "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>",                     desc = "Prev Diagnostic" },
-    { "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>",                         desc = "CodeLens Action" },
-    { "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<cr>",                    desc = "Quickfix" },
-    { "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>",                           desc = "Rename" },
+    { "<leader>lh", "<cmd>lua vim.diagnostic.open_float(0, { severity = vim.diagnostic.severity.HINT })<cr>", desc = "Hints" },
+    { "<leader>li", "<cmd>LspInfo<cr>",                                                                       desc = "Info" },
+    { "<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>",                                                desc = "Next Diagnostic" },
+    { "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>",                                                desc = "Prev Diagnostic" },
+    { "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>",                                                    desc = "CodeLens Action" },
+    { "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<cr>",                                               desc = "Quickfix" },
+    { "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>",                                                      desc = "Rename" },
   }
 
   wk.add {
