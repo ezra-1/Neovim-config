0041eb7305c0bd962f6a26ba541170b834718aff
Ezra Lawrence <ezralawrence03@gmail.com>
2026-02-23 18:38:46 -0500

    feat(ui): add curated colorscheme collection

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+local M = {
+  "folke/tokyonight.nvim",
+  "rose-pine/neovim",
+  "catppuccin/nvim",
+  "EdenEast/nightfox.nvim",
+  "rebelot/kanagawa.nvim",
+  "navarasu/onedark.nvim",
+  "sainnhe/everforest",
+  "sainnhe/gruvbox-material",
+  "ellisonleao/gruvbox.nvim",
+  "projekt0n/github-nvim-theme",
+  "Mofiqul/dracula.nvim",
+  "ayu-theme/ayu-vim",
+  "shaunsingh/solarized.nvim",
+  "shaunsingh/nord.nvim",
+  "martinsione/darkplus.nvim",
+  "cpea2506/one_monokai.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+}
+
+return M
