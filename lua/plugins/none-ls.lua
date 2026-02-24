9abd621685301b7e66a1f5780a0f317c7f8b196d
Ezra Lawrence <ezralawrence03@gmail.com>
2026-02-24 00:56:59 -0500

    feat(formatting): integrate none-ls with stylua, prettier, black, and isort

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,39 @@
+local M = {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
+
+function M.config()
+  local null_ls = require("null-ls")
+
+  local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
+  local code_actions = null_ls.builtins.code_actions
+  local completion = null_ls.builtins.completion
+
+  null_ls.setup({
+    debug = true,
+    sources = {
+      -- 🧹 Formatting
+      formatting.stylua,
+      formatting.prettier,
+      formatting.black,
+      formatting.isort,
+
+      -- 🔍 Diagnostics
+      -- diagnostics.flake8,
+      diagnostics.mypy,
+      -- diagnostics.luacheck,
+
+      -- 💡 Code Actions
+      code_actions.gitsigns,
+
+      -- ✍️ Completion
+      completion.spell,
+    },
+  })
+end
+
+return M
