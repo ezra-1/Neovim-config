7c5681bb5965833bed18b3bfb13f78a16bd65e8f
Ezra Lawrence <ezralawrence03@gmail.com>
2026-03-09 01:46:53 -0400

    feat(plugins): configure which-key for keybinding hints

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,54 @@
+local M = {
+  "folke/which-key.nvim",
+}
+
+function M.config()
+  local which_key = require "which-key"
+  which_key.setup {
+    defaults = {
+      mode = "n",
+      prefix = "<leader>",
+    },
+    spec = {
+      { "<leader>w", "<cmd>confirm w<CR>", desc = "Save" },
+      { "<leader>q", "<cmd>confirm q<CR>", desc = "Quit" },
+      { "<leader>h", "<cmd>nohlsearch<CR>", desc = "NOHL" },
+      { "<leader>v", "<cmd>vsplit<CR>", desc = "Split" },
+      { "<leader>aN", "<cmd>tabnew %<cr>", desc = "New Tab" },
+      { "<leader>ah", "<cmd>-tabmove<cr>", desc = "Move Left" },
+      { "<leader>al", "<cmd>+tabmove<cr>", desc = "Move Right" },
+      { "<leader>an", "<cmd>$tabnew<cr>", desc = "New Empty Tab" },
+      { "<leader>ao", "<cmd>tabonly<cr>", desc = "Only" },
+    },
+    plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = false,
+        windows = false,
+        nav = false,
+        z = false,
+        g = false,
+      },
+    },
+    win = {
+      padding = { 2, 2, 2, 2 },
+      border = "rounded",
+    },
+    -- ignore_missing = true,
+    show_help = false,
+    show_keys = false,
+    disable = {
+      buftypes = {},
+      filetypes = { "TelescopePrompt" },
+    },
+  }
+end
+
+return M
