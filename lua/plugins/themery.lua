67ea27a59bb1a7ad316f650046a0478c625c6faa
Ezra Lawrence <ezralawrence03@gmail.com>
2026-03-17 02:40:27 -0400

    feat(plugins): add Themery.nvim with themes and live preview

---
+++ lua/plugins/themery.lua
@@ -0,0 +1,28 @@
+local M = {
+  "zaldih/themery.nvim",
+  lazy = false,
+  priority = 999,
+}
+
+function M.config()
+  local themery = require("themery")
+
+  themery.setup({
+    themes = {
+      { name = "Tokyo Night (Moon)",  colorscheme = "tokyonight-moon" },
+      { name = "Tokyo Night (Storm)", colorscheme = "tokyonight-storm" },
+      { name = "Catppuccin Mocha",    colorscheme = "catppuccin-mocha" },
+      { name = "Gruvbox",             colorscheme = "gruvbox" },
+      { name = "Everforest",          colorscheme = "everforest" },
+      { name = "Rose Pine",           colorscheme = "rose-pine" },
+      { name = "Nord",                colorscheme = "nord" },
+      { name = "Dracula",             colorscheme = "dracula" },
+    },
+    livePreview = true,
+  })
+
+  -- keymap
+  vim.keymap.set("n", "<leader>c", "<cmd>Themery<cr>", { desc = "Colorscheme Picker" })
+end
+
+return M
