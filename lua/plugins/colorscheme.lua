a1c83419086cfa7d1837c338245e9caa5b6f60e5
Ezra Lawrence <ezralawrence03@gmail.com>
2026-03-17 02:36:06 -0400

    chore(plugins): add multiple colorscheme plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,20 @@
 local M = {
-  "catppuccin/nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "EdenEast/nightfox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "sainnhe/everforest" },
+  { "sainnhe/gruvbox-material" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  { "Mofiqul/dracula.nvim" },
+  { "ayu-theme/ayu-vim" },
+  { "shaunsingh/solarized.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "martinsione/darkplus.nvim" },
+  { "cpea2506/one_monokai.nvim" },
 }
-
-function M.config()
-  vim.cmd.colorscheme "catppuccin-mocha"
-end
 
 return M
