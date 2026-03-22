c48553cfc2a71e0dd1c7b544122fffbd64b8c15a
Ezra Lawrence <ezralawrence03@gmail.com>
2026-03-22 17:52:01 -0400

    feat(plugins): add render-markdown for markdown and codecompanion buffers

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+local M = {
+  "MeanderingProgrammer/render-markdown.nvim",
+   ft = { "markdown", "codecompanion" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
+
+function M.config()
+  require("render-markdown").setup({})
+end
+
+return M
