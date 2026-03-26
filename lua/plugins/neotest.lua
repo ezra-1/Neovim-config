16eff44ddebc1eb8ef5ec82af7a10da80c5f84a5
Ezra Lawrence <ezralawrence03@gmail.com>
2026-03-26 03:28:15 -0400

    refactor(neotest): setup multi-adapters with keymaps and output

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,10 +3,8 @@ local M = {
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-treesitter/nvim-treesitter",
-    -- general tests
-    "vim-test/vim-test",
-    "nvim-neotest/neotest-vim-test",
-    -- language specific tests
+
+    -- language specific
     "marilari88/neotest-vitest",
     "nvim-neotest/neotest-python",
     "nvim-neotest/neotest-plenary",
@@ -18,50 +16,52 @@ local M = {
 }
 
 function M.config()
-  local wk = require "which-key"
-  wk.add {
-    { "<leader>tt", "<cmd>lua require'neotest'.run.run()<cr>", desc = "Test Nearest" },
-    { "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "Test File" },
-    { "<leader>td", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", desc = "Debug Test" },
-    { "<leader>ts", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Test Stop" },
-    { "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>", desc = "Attach Test" },
-    { "<leader>to", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Toggle Test Summary" },
-  }
+  local wk = require("which-key")
 
-  ---@diagnostic disable: missing-fields
-  require("neotest").setup {
+  wk.add({
+    { "<leader>tt", function() require("neotest").run.run() end, desc = "Test Nearest" },
+    { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Test File" },
+    { "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug Test" },
+    { "<leader>ts", function() require("neotest").run.stop() end, desc = "Stop Test" },
+    { "<leader>ta", function() require("neotest").run.attach() end, desc = "Attach" },
+    { "<leader>to", function() require("neotest").summary.toggle() end, desc = "Summary" },
+  })
+
+  require("neotest").setup({
     adapters = {
-      require("neotest-python") {
+      require("neotest-python")({
         dap = { justMyCode = false },
-      },
-      require("neotest-vitest") {
+      }),
+
+      require("neotest-vitest")({
         vitestCommand = "npx vitest",
         filterDir = function(name)
           return name ~= "node_modules"
         end,
-      },
-      require("neotest-jest") {
+      }),
+
+      require("neotest-jest")({
         jestCommand = "npm test --",
         jestConfigFile = "jest.config.ts",
         env = { CI = true },
         cwd = function() return vim.fn.getcwd() end,
-      },
+      }),
+
       require("neotest-zig"),
       require("neotest-rust"),
       require("neotest-plenary"),
       require("neotest-bash"),
-      require("neotest-vim-test") {
-        ignore_file_types = { "python", "vim", "lua", "javascript", "typescript" },
-      },
     },
+
     icons = {
       running = "",
       passed = "",
       failed = "",
       skipped = "",
     },
+
     output = { open_on_run = true },
-  }
+  })
 end
 
 return M
