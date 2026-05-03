216705a89e2cf67a23464dd289e78d335f9bf1d7
Ezra <ezralawrence03@gmail.com>
2026-05-03 01:38:54 -0500

    fix(ufo): disable folding for snacks_dashboard filetype

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -8,14 +8,17 @@ local M = {
 
 function M.config()
   -- Global fold settings (required for ufo to work properly)
-  vim.o.foldcolumn = "1"       -- Show fold column
-  vim.o.foldlevel = 99         -- Start with all folds open
+  vim.o.foldcolumn = "1" -- Show fold column
+  vim.o.foldlevel = 99   -- Start with all folds open
   vim.o.foldlevelstart = 99
   vim.o.foldenable = true
 
   -- Optional: minimal setup with default providers
   require("ufo").setup({
     provider_selector = function(bufnr, filetype, buftype)
+      if filetype == "snacks_dashboard" then
+        return ""
+      end
       return { "treesitter", "indent" }
     end,
   })
