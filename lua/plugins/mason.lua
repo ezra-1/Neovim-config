ee8d64feeecc695df5606b0c2dc874a27ad192eb
Ezra <ezralawrence03@gmail.com>
2026-05-08 00:03:11 -0500

    feat(mason): add mason-tool-installer for automatic formatter setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,31 +1,49 @@
 local M = {
-  "mason-org/mason-lspconfig.nvim",
-  dependencies = {
-    "mason-org/mason.nvim",
-  },
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 }
 
 function M.config()
-  local servers = {
-    "lua_ls",
-    "cssls",
-    "html",
-    "ts_ls",
-    "pyright",
-    "bashls",
-    "jsonls",
-    "jdtls",
-  }
+	local servers = {
+		"lua_ls",
+		"cssls",
+		"html",
+		"ts_ls",
+		"pyright",
+		"bashls",
+		"jsonls",
+		"jdtls",
+	}
 
-  require("mason").setup {
-    ui = {
-      border = "rounded",
-    },
-  }
+	local formatters = {
+		"stylua",
+		"prettier",
+		"black",
+		"shfmt",
+		"nixfmt",
+		"google-java-format",
+	}
 
-  require("mason-lspconfig").setup {
-    ensure_installed = servers,
-  }
+	require("mason").setup({
+		ui = {
+			border = "rounded",
+		},
+	})
+
+	require("mason-lspconfig").setup({
+		ensure_installed = servers,
+	})
+
+	require("mason-tool-installer").setup({
+		ensure_installed = formatters,
+
+		auto_update = false,
+		run_on_start = true,
+		start_delay = 3000,
+	})
 end
 
 return M
